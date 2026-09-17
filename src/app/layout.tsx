5b6844b47ac27754c9a1f79316a37f9161e204e3
Qin Jiangban <tanjiangban@gmail.com>
2026-09-17 15:53:41 +0800

    hy3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import SiteHeader from "@/components/SiteHeader";
-import { LanguageProvider } from "@/components/LanguageProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,10 +34,8 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="flex min-h-full flex-col">
-        <LanguageProvider>
-          <SiteHeader />
-          <div className="flex min-h-0 flex-1 flex-col">{children}</div>
-        </LanguageProvider>
+        <SiteHeader />
+        <div className="flex min-h-0 flex-1 flex-col">{children}</div>
       </body>
     </html>
   );
